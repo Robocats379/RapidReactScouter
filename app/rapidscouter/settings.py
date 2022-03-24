578412fa47544db693020638a64d190f98ed2a7c
Jarrett Duskey <jarrett@duskey.io>
2022-03-24 10:13:23 -0400

    Change Static

--- app/rapidscouter/settings.py
+++ app/rapidscouter/settings.py
@@ -124,9 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    str(BASE_DIR + '/globalstatic')
-]
-STATIC_ROOT = str(BASE_DIR + '/static')
-STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
