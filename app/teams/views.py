08c5612f3db7cbeb3acb2d98fc8490707e96f122
Jarrett Duskey <jarrett@duskey.io>
2022-03-26 07:55:20 -0400

    Fix match ordering

--- app/teams/views.py
+++ app/teams/views.py
@@ -9,7 +9,7 @@ def team_list(request):
 
 def team_detail(request, pk):
     team = Team.objects.get(team_number=pk)
-    matches = ScoutedMatch.objects.filter(team=team)
+    matches = ScoutedMatch.objects.filter(team=team).order_by('match__number')
     notes = matches.exclude(notes="")
     context = {
         'team': team,
